3afa953e9bc3d86ddd403fc831233d54d41b0bb8
luchom77 <luchomooreventi@yahoo.com>
2022-12-24 17:48:53 -0300

    24/10/2022

--- script.js
+++ script.js
@@ -21,3 +21,42 @@
     $menu.classList.remove("is-active");
   });
 })(document);
+
+/* **** Contact Form **** */
+
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact.form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("http://formsubmit.co/ajax/luchomooreventi@yahoo.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        $loader.classList.add("none");
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrio un erro al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+        $loader.classList.add("none");
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
+  });
+})(document);
